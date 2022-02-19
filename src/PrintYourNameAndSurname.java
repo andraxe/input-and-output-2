41ef1998ad09a8069b57a0a323363272e1fa2af9
andraxe <99595235+andraxe@users.noreply.github.com>
2022-02-19 23:15:33 +0100

    Initial commit

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,11 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+
+
+        String name= "Andrea"; String surname = " Bottino";
+
+
+         System.out.println(name + " " + surname);
+
+    }
+}
